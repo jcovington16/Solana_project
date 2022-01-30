910e625ab411dbc1ef6c4d0b683db9679b69db21
jcovington16 <covingtonjoshua7@gmail.com>
2022-01-29 21:26:04 -0700

    chore: need to fix the ConnectButton component to ensure I can separate functionality out of the main app.js file. will come back to this.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React ,{ useEffect } from 'react';
+import React ,{ useEffect, useState } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
-import ConnectButton from './components/connectButton';
+// import ConnectButton from './components/connectButton';
 import './App.css';
 
 
@@ -10,6 +10,8 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  // Setting State
+  const [walletAddress, setWalletAddress] = useState(null)
 
   const checkIfWalletIsConnected = async() => {
     try {
@@ -22,7 +24,8 @@ const App = () => {
           // The solana object gives us a function that will let us connect to our wallet
           const res = await solana.connect({ onlyIfTrusted: true });
 
-          console.log(`Connected with Public Key: ${res.publicKey.toString()}`);
+          // Setting the user's publicKey in state to be used at a later time
+          setWalletAddress(res.publicKey.toString());
         }
       } else {
         alert("Solana object not found! Get a Phantom Wallet 👻");
@@ -31,6 +34,22 @@ const App = () => {
       console.log(error)
     }
   };
+
+  // Will connect this with the ConnectButton component.
+
+  const connectWallet = async () => {
+    const {solana} = window;
+
+    if (solana) {
+      const res = await solana.connect();
+      console.log(`Connected with Public Key: ${res.publicKey.toString()}`);
+      setWalletAddress(res.publicKey.toString());
+    }
+  }
+
+  const renderNotConnectedContainer = () => (
+    <button className='cta-button connect-wallet-button' onClick={() => connectWallet()}>Connect to Phantom Wallet</button>
+  );
 
   useEffect(() => {
     const onLoad = async() => {
@@ -42,13 +61,14 @@ const App = () => {
 
   return (
     <div className="App">
-      <div className="container">
+      <div className={walletAddress ? 'authed-container' : 'container'}>
         <div className="header-container">
           <p className="header">👾 Anime Post Board 👾</p>
           <p className="sub-text">
-            View your Anime collection in the metaverse ✨
+            View your Anime collection in the metaverse 🚀
           </p>
-          <ConnectButton />
+          {/* will add this to the ConnectWallet Component in order to clean App.js up */}
+          {!walletAddress && renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
--- src/components/connectButton.jsx
+++ src/components/connectButton.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import '../App.css';
 
 const ConnectButton = () => {
     const connectWallet = async () => {}
 
-    const renderNotConnectedContainer = () => {
-        <button className='cta-button connect-wallet-button' onClick={connectWallet}>
-        Connect to Wallet
-      </button>
-    }
+    // const renderNotConnectedContainer = () => {
+    //     <button className='cta-button connect-wallet-button' onClick={connectWallet}>
+    //     Connect to Wallet
+    //   </button>
+    // }
 
   return (
   <div>
-      {renderNotConnectedContainer()}
+      {/* {renderNotConnectedContainer()} */}
+      <button className='cta-button connect-wallet-button' onClick={connectWallet}>
+        Connect to Phantom Wallet
+      </button>
   </div>);
 }
 

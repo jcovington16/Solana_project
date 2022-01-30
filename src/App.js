b9ca54b011b086fcb5546cee609a49b12314f2b5
jcovington16 <covingtonjoshua7@gmail.com>
2022-01-29 18:28:48 -0700

    first commit of my web3 solana project from buildspace.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
+import React ,{ useEffect } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
+
+
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+
+  const checkIfWalletIsConnected = async() => {
+    try {
+      const {solana} = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log("👻 Phantom wallet found!")
+        }
+      } else {
+        alert("Solana object not found! Get a Phantom Wallet 👻");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  useEffect(() => {
+    const onLoad = async() => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
+          <p className="header">👾 Anime Post Board 👾</p>
           <p className="sub-text">
-            View your GIF collection in the metaverse ✨
+            View your Anime collection in the metaverse ✨
           </p>
         </div>
         <div className="footer-container">

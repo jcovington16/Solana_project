466a68ea15bdebc619b2581bc51b46c961489273
jcovington16 <covingtonjoshua7@gmail.com>
2022-01-30 16:48:53 -0700

    chore: users can connect with their phantom wallet and see the wall full of gifs. Will continue to add components to come back and clean up at a later date.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React ,{ useEffect, useState } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
-// import ConnectButton from './components/connectButton';
+// import ConnectButton from './components/ConnectButton';
+//import Test_Gifs from './components/Test_Gifs';
 import './App.css';
 
 
@@ -8,6 +9,13 @@ import './App.css';
 // Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TEST_GIFS = [
+
+  'https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp',
+  'https://media3.giphy.com/media/L71a8LW2UrKwPaWNYM/giphy.gif?cid=ecf05e47rr9qizx2msjucl1xyvuu47d7kf25tqt2lvo024uo&rid=giphy.gif&ct=g',
+  'https://media4.giphy.com/media/AeFmQjHMtEySooOc8K/giphy.gif?cid=ecf05e47qdzhdma2y3ugn32lkgi972z9mpfzocjj6z1ro4ec&rid=giphy.gif&ct=g',
+  'https://i.giphy.com/media/PAqjdPkJLDsmBRSYUp/giphy.webp'
+]
 
 const App = () => {
   // Setting State
@@ -51,6 +59,18 @@ const App = () => {
     <button className='cta-button connect-wallet-button' onClick={() => connectWallet()}>Connect to Phantom Wallet</button>
   );
 
+  const renderConnectedContainer = () => (
+    <div className='connected-container'>
+      <div className="gif-grid">
+          {TEST_GIFS.map(gif => (
+              <div className="gif-item" key={gif}>
+                  <img src={gif} alt={gif} />
+              </div>
+          ))}
+      </div>
+    </div>
+  );
+
   useEffect(() => {
     const onLoad = async() => {
       await checkIfWalletIsConnected();
@@ -69,6 +89,8 @@ const App = () => {
           </p>
           {/* will add this to the ConnectWallet Component in order to clean App.js up */}
           {!walletAddress && renderNotConnectedContainer()}
+          {/* Add the inverse here */}
+          {walletAddress && renderConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
